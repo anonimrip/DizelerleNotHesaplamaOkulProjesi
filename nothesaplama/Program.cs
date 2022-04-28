848ab893d6d7d56643d809c2c2b5942d3a6f59d8
EREN FELEKOGLU <EREN FELEKOGLU@BIL-12>
2022-04-28 10:28:10 +0300

    Proje dosyası ekle.

---
+++ nothesaplama/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace nothesaplama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            int[] yazili_notlari = {100,100,100};
+
+            Console.WriteLine("1. Yazılı Notunuz: " + yazili_notlari[0]);
+            Console.WriteLine("2. Yazılı Notunuz: " + yazili_notlari[1]);
+            Console.WriteLine("3. Yazılı Notunuz: " + yazili_notlari[2]);
+
+
+            int hesapla =(yazili_notlari[0] + yazili_notlari[1] + yazili_notlari[2]) /3 ;
+            Console.WriteLine(hesapla);
+        }
+    }
+}
